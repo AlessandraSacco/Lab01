d428b27507217742484d3e802146aaa409cb9d60
Alessandra <Alessandra@LAPTOP-DT917UDD>
2020-03-24 15:45:33 +0100

    Modifiche Programma

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -2,6 +2,7 @@ package it.polito.tdp.parole;
 
 import it.polito.tdp.parole.model.Parole;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -9,6 +10,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class FXMLController {
 	
@@ -31,15 +33,56 @@ public class FXMLController {
 
     @FXML
     private Button btnReset;
+    
+    @FXML
+    private Button btnCanccella;
+    
+    private Parole p = new Parole();
 
     @FXML
     void doInsert(ActionEvent event) {
-    	// TODO
+    	String inserisci = txtParola.getText();
+    	p.addParola(inserisci);
+    	String res="";
+    	for(String inserisci1: p.getElenco()) {
+    		res+= inserisci1+"\n";
+    	}
+    	txtParola.clear();
+    	txtResult.setText(res);
+    
     }
+    @FXML
+    void doSelect(MouseEvent event) {
+     txtResult.getSelectedText();
+     System.out.println(  txtResult.getSelectedText());
+   
+    }
+    
+    
+    @FXML
+    void doCancella(ActionEvent event) {
+    	String s1= txtResult.getSelectedText(); //s1 è la parola selezionata
+    	String res1=""; 
+    	  p.resetonly1(s1); //cancello s1
+    	for(String s2: p.getElenco()) {
+    		res1+= s2+"\n";
+    		//salvo nomi della lista in res1
+    		
+    	}
+    	 
+    	  txtResult.setText(res1);
+    	  System.out.println(s1);
+    	
+   
+    }
+    
+   
 
     @FXML
     void doReset(ActionEvent event) {
-    	// TODO
+    	
+    	txtResult.clear();
+    	p.reset();
     }
 
     @FXML
@@ -47,8 +90,8 @@ public class FXMLController {
         assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCanccella != null : "fx:id=\"btnCanccella\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
-
         elenco = new Parole() ;
     }
 }
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,35 @@
 package it.polito.tdp.parole.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import com.zaxxer.hikari.util.SuspendResumeLock;
+
 public class Parole {
+	List<String> elenco = new ArrayList<>();
 		
 	public Parole() {
 		//TODO
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		elenco.add(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		Collections.sort(elenco,new ConfrontaNomi());
+		//System.out.println(elenco);
+		return elenco;
 	}
 	
 	public void reset() {
-		// TODO
+		elenco.removeAll(elenco);
+	}
+	
+	public void resetonly1(String s) {
+		elenco.remove(s);
+	    
 	}
 
 }
